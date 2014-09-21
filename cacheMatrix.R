cbdb1292ffcb024eef841ae64f56a2cc59652e5a
phadkeunmesh <phadke.unmesh@gmail.com>
2014-09-21 17:29:46 +0530

    unmesh

--- cacheMatrix.R
+++ cacheMatrix.R
@@ -3,6 +3,9 @@
 
 ## Write a short comment describing this function
 
+##This function is very similar to the cached mean function that was given as an example.
+##set function sets the matrix,get is used to get the matrix stored. GetInverse fn is used to get hte cached mean and setInverse is used to set the mean.
+
 makeCacheMatrix <- function(x = matrix()) {
   i<-NULL
   set<-function(y){
@@ -19,6 +22,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+##This function first checks whether the inverse has been cached or not . If it has been cached it uses getInverse.
+##Else inverse is calculated using solve() and set using setInverse.
 
 cacheSolve <- function(x, ...) {
   
